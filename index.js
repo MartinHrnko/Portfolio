0d4c63c0c22a2335f18c1d7567fdb688e682c008
MartinHrnko <120335599+MartinHrnko@users.noreply.github.com>
2024-08-26 19:40:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static("public")); //linking other files such as CSS
+app.use(bodyParser.urlencoded({ extended:true })); //ability to open new files
+
+// MAIN PAGE OPEN
+app.get("/", (req, res) => {
+    try {
+        res.render("index.ejs");
+    } catch {
+        console.log("Failed to make a request :", error.message);
+        res.status(404).send("error.message");
+    }
+});
+
+// requesting a new symbol from API and showing it into a home page
+let portfolio = []
+
+// app.get("/home"), (req, res) => {
+//     res.render("index.ejs", { data: portfolio });
+//     console.log(portfolio);
+// }
+
+app.post("/", async (req, res) => {
+    const companyName = req.body.symbol;
+    try {
+        const API = "?apikey=bJcGHJL92cISqOKjpLudNTtGaizSMMGr";
+        const response = await axios.get("https://financialmodelingprep.com/api/v3/profile/" + companyName + API );
+        const reply = response.data;
+        portfolio.push(reply[0]);
+        res.render("index.ejs", { data: portfolio });
+    } catch {
+        console.log("Failed to make a request :");
+        res.status(404).send("error.message");
+    }
+});
+
+
+// This will open a new page for the symbol of investment, that you have openned
+app.get("/portfolio/:lineSymbol", (req, res) => {
+    const requestedLine = req.params.lineSymbol;
+    res.render("portfolio.ejs", { symbol: requestedLine });
+})
+
+app.get("/delete/:lineSymbol", (req, res) => {
+    const requestedLine = req.params.lineSymbol;
+    portfolio = portfolio.filter(function (line) {
+        return (line.symbol) !== requestedLine;
+    });
+    res.redirect("/");
+})
+
+
+// Opening port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
